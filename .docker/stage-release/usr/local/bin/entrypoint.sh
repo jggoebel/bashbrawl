2e98157293ead15f665370f6cba806494c638294
Jan-Gerrit Goebel <Jan-Gerrit.Goebel@sva.de>
2024-05-28 09:45:26 +0200

    Do not append the protocol to the server

--- .docker/stage-release/usr/local/bin/entrypoint.sh
+++ .docker/stage-release/usr/local/bin/entrypoint.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-# check all args for the insecure flag
-if [ "${*#*--insecure}" = "$*" ]; then
-    HF_SERVER=https://$HF_SERVER
-else
-    HF_SERVER=http://$HF_SERVER
-fi
+sed -i'' "s|#####SERVER#####|$SERVER|g;" /usr/share/nginx/html/main*.js*
 
-sed -i'' "s|#####HF_SERVER#####|$HF_SERVER|g;" /usr/share/nginx/html/main*.js*
-
-echo "Configured with HF_SERVER=$HF_SERVER"
+echo "Configured with SERVER=$SERVER"
 
 nginx -g 'daemon off;' # overriding nginx default startup
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  server: '#####HF_SERVER#####',
+  server: '#####SERVER#####',
 };
